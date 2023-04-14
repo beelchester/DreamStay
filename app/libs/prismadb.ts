69665f996bd8e87e896eee429e6fe72d24d6a818
Sahil Yeole <sahilyeole93@gmail.com>
2023-04-14 08:01:24 +0530

    add: nextauth, register api

--- app/libs/prismadb.ts
+++ app/libs/prismadb.ts
@@ -1,11 +1,15 @@
 import {PrismaClient} from '@prisma/client'
 
 declare global {
-    var prisma: PrismaClient | undefined
+    var prisma: PrismaClient | undefined 
     }
 
 const client = globalThis.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV === 'development') globalThis.prisma = client
+
+    client.user.findMany().then((users) => {
+        console.log(users)
+    })
 
 export default client
