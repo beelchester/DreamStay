6bad0dbf751cbbac1a38b7d6f10741d7bbbe154e
Sahil Yeole <sahilyeole93@gmail.com>
2023-04-24 16:55:43 +0530

    fix: getListingById type error

--- app/actions/getListingById.ts
+++ app/actions/getListingById.ts
@@ -20,8 +20,15 @@ export default async function getListingById(params: IParams) {
         return null;
     }
 
-    return listing;
-
+return {
+   ...listing,
+      createdAt: listing.createdAt?.toString() || '',
+      user: {
+        ...listing.user,
+        createdAt: listing.user.createdAt?.toString() || '',
+        updatedAt: listing.user.updatedAt?.toString() || '',
+        emailVerified: listing.user.emailVerified?.toString() || null,   }
+    };
     } catch (error) {
         
     }
